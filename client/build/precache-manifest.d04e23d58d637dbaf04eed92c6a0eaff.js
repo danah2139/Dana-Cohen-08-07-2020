69ed18401a0d27631f0387ab57147ea76adc4d6d
dana cohen <danah2139@gmail.com>
2020-07-12 23:44:31 +0300

    Deployment

---
+++ client/build/precache-manifest.d04e23d58d637dbaf04eed92c6a0eaff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f497568d7695937bdf2964bdcbf6a78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ac85a55d8f56797f078",
+    "url": "/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "9ac85a55d8f56797f078",
+    "url": "/static/js/2.0d98b389.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.0d98b389.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0254e8e1054517dba40b",
+    "url": "/static/js/main.d0ff3465.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
